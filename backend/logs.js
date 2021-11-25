7ecd93fd346c7c714a90dd5c89948addaacc100c
omersahin3 <omersahine@hotmail.com>
2021-11-25 11:14:21 +0300

    know a little good logs code

--- backend/logs.js
+++ backend/logs.js
@@ -1,32 +1,31 @@
-const http = require('http');
 const url = `https://api.openweathermap.org/data/2.5/weather?q=istanbul&appid=49cc8c821cd2aff9af04c9f98c36eb74`
 const request = require('request');
 const db = require("./src/models");
 const Log = db.log;
 const ServerService = db.server_service;
 
-function logs() {
-  const server = http.createServer((req, res) => {
-    request(url, function reqq(err, response, body) {
-      if(err){
-        res.end(JSON.stringify({ message: 'error:', err }));
-      } else {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        // res.end(JSON.stringify({ data: response.statusCode }));
-        console.log(response.statusCode)
-        // Log.create({
-        //   status:true,
-        //   server_services_id: 1
-        // }).then(log => {
-        //   console.log(log)
-        // });
-        ServerService.findAll({ 
-          include: ["logs"]
-        }).then(data => {
-          res.end(JSON.stringify({ data: data }));
+function intervalFunc() {
+  request(url, function (err, response, body) {
+    if(err){
+      console.log(JSON.stringify({ message: 'error:', err }));
+    } else {
+      console.log(response.statusCode)
+      if(response.statusCode >= 200 && response.statusCode <= 299){
+        Log.create({
+          status:true,
+          server_services_id: 1
         });
       }
-    });
+      else{
+        Log.create({
+          status:false,
+          server_services_id: 1
+        });
+      }
+      ServerService.findAll({ 
+        include: ["logs"]
+      });
+    }
   });
 }
-module.exports = logs;
+module.exports = intervalFunc;
--- backend/server.js
+++ backend/server.js
@@ -6,48 +6,21 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
+
 const http = require('http');
-const url = `https://api.openweathermap.org/data/2.5/weather?q=istanbul&appid=49cc8c821cd2aff9af04c9f98c36eb74`
-const request = require('request');
 const db = require("./src/models");
-const Log = db.log;
+const intervalFunc = require("./logs");
 const ServerService = db.server_service;
+
 const server = http.createServer((req, res) => {
-  request(url, function (err, response, body) {
-    if(err){
-      res.end(JSON.stringify({ message: 'error:', err }));
-    } else {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      console.log(response.statusCode)
-      if(response.statusCode >= 200 && response.statusCode <= 299){
-        Log.create({
-          status:true,
-          server_services_id: 1
-        });
-      }
-      else{
-        Log.create({
-          status:false,
-          server_services_id: 1
-        });
-      }
-      ServerService.findAll({ 
-        include: ["logs"]
-      }).then(data => {
-        res.end(JSON.stringify({ data: data }));
-      });
-    }
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  ServerService.findAll({ 
+    include: ["logs"]
+  }).then(data => {
+    res.end(JSON.stringify({ data: data }));
   });
 });
 
 server.listen(8009);
-
-setInterval(() => {
-  // Adapting a keep-alive agent
-  http.get('http://localhost:8009', (res) => {
-    res.on('data', (data) => {
-      // Do nothing
-    });
-  });
-}, 300000);
+setInterval(intervalFunc, 300000);
 // 300000
